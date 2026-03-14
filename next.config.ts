7bbba675ffd29c639f50e8b5366c76ea0108b107
markos900313 <markos900313@users.noreply.github.com>
2026-03-14 21:27:38 +0100

    Fix NextConfig type error to allow deployment

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  devIndicators: {
-    appIsrStatus: false,
-    buildActivity: false,
-    staticIndicator: false,
-  },
+  /* config options here */
 };
 
 export default nextConfig;
