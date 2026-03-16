ddfe858b60df86c05a3618b9cf52c0df45282046
markos900313 <markos900313@users.noreply.github.com>
2026-03-16 13:09:21 +0100

    Update canonical URL to WWW in sitemap and robots

--- app/robots.ts
+++ app/robots.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
       allow: '/',
       disallow: '/api/',
     },
-    sitemap: 'https://sffalcon.com/sitemap.xml',
+    sitemap: 'https://www.sffalcon.com/sitemap.xml',
   }
 }
